d0ed8b1cd7a6f1918454ebeaff0916d6bda3ee32
James Cook <james@jaminit.co.uk>
2021-11-23 22:12:17 +0000

    chore: line endings

--- src/api.ts
+++ src/api.ts
@@ -4,52 +4,52 @@
  */
 import { ClasschartsClient } from '.'
 import {
-    ActivityResponse,
-    BehaviourResponse,
-    GetActivityOptions,
-    GetBehaviourOptions,
-    GetHomeworkOptions,
-    GetLessonsOptions,
-    HomeworksResponse,
-    LessonsResponse,
-    Student,
+	ActivityResponse,
+	BehaviourResponse,
+	GetActivityOptions,
+	GetBehaviourOptions,
+	GetHomeworkOptions,
+	GetLessonsOptions,
+	HomeworksResponse,
+	LessonsResponse,
+	Student,
 } from './types'
 export async function getStudentInfo(
-    studentCode: string,
-    dateOfBirth: string | null
+	studentCode: string,
+	dateOfBirth: string | null
 ): Promise<Student> {
-    const client = new ClasschartsClient(studentCode, dateOfBirth)
-    return await client.getStudentInfo()
+	const client = new ClasschartsClient(studentCode, dateOfBirth)
+	return await client.getStudentInfo()
 }
 export async function getActivity(
-    studentCode: string,
-    dateOfBirth: string | null,
-    options: GetActivityOptions | null
+	studentCode: string,
+	dateOfBirth: string | null,
+	options: GetActivityOptions | null
 ): Promise<ActivityResponse> {
-    const client = new ClasschartsClient(studentCode, dateOfBirth)
-    return await client.getActivity(options)
+	const client = new ClasschartsClient(studentCode, dateOfBirth)
+	return await client.getActivity(options)
 }
 export async function getBehaviour(
-    studentCode: string,
-    dateOfBirth: string | null,
-    options: GetBehaviourOptions | null
+	studentCode: string,
+	dateOfBirth: string | null,
+	options: GetBehaviourOptions | null
 ): Promise<BehaviourResponse> {
-    const client = new ClasschartsClient(studentCode, dateOfBirth)
-    return await client.getBehaviour(options)
+	const client = new ClasschartsClient(studentCode, dateOfBirth)
+	return await client.getBehaviour(options)
 }
 export async function listHomeworks(
-    studentCode: string,
-    dateOfBirth: string | null,
-    options: GetHomeworkOptions | null
+	studentCode: string,
+	dateOfBirth: string | null,
+	options: GetHomeworkOptions | null
 ): Promise<HomeworksResponse> {
-    const client = new ClasschartsClient(studentCode, dateOfBirth)
-    return await client.listHomeworks(options)
+	const client = new ClasschartsClient(studentCode, dateOfBirth)
+	return await client.listHomeworks(options)
 }
 export async function getLessons(
-    studentCode: string,
-    dateOfBirth: string | null,
-    options: GetLessonsOptions
+	studentCode: string,
+	dateOfBirth: string | null,
+	options: GetLessonsOptions
 ): Promise<LessonsResponse> {
-    const client = new ClasschartsClient(studentCode, dateOfBirth)
-    return await client.getLessons(options)
+	const client = new ClasschartsClient(studentCode, dateOfBirth)
+	return await client.getLessons(options)
 }
--- src/release.ts
+++ src/release.ts
@@ -5,25 +5,25 @@ import fs from 'fs'
 // It will not be included in the npm package.
 
 function main() {
-    const source = fs
-        .readFileSync(__dirname + '/../package.json')
-        .toString('utf-8')
-    const sourceObj = JSON.parse(source)
-    sourceObj.scripts = {}
-    sourceObj.devDependencies = {}
-    if (sourceObj.main.startsWith('dist/')) {
-        sourceObj.main = sourceObj.main.slice(5)
-    }
-    fs.writeFileSync(
-        __dirname + '/package.json',
-        Buffer.from(JSON.stringify(sourceObj, null, 2), 'utf-8')
-    )
-    fs.writeFileSync(
-        __dirname + '/version.txt',
-        Buffer.from(sourceObj.version, 'utf-8')
-    )
-    fs.copyFileSync(__dirname + '/../.npmignore', __dirname + '/.npmignore')
-    fs.copyFileSync(__dirname + '/../README.MD', __dirname + '/README.MD')
+	const source = fs
+		.readFileSync(__dirname + '/../package.json')
+		.toString('utf-8')
+	const sourceObj = JSON.parse(source)
+	sourceObj.scripts = {}
+	sourceObj.devDependencies = {}
+	if (sourceObj.main.startsWith('dist/')) {
+		sourceObj.main = sourceObj.main.slice(5)
+	}
+	fs.writeFileSync(
+		__dirname + '/package.json',
+		Buffer.from(JSON.stringify(sourceObj, null, 2), 'utf-8')
+	)
+	fs.writeFileSync(
+		__dirname + '/version.txt',
+		Buffer.from(sourceObj.version, 'utf-8')
+	)
+	fs.copyFileSync(__dirname + '/../.npmignore', __dirname + '/.npmignore')
+	fs.copyFileSync(__dirname + '/../README.MD', __dirname + '/README.MD')
 }
 
 main()
