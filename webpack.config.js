03f061134891de7f9ed47d194a3fba203260a430
James Cook <james@jaminit.co.uk>
2022-03-24 18:55:01 +0000

    feat: webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index",
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  output: {
+    library: {
+      name: "classchartsapi",
+      type: "umd",
+    },
+    sourceMapFilename: "classcharts-api.map",
+    path: path.resolve(__dirname, "build"),
+    filename: "classcharts-api.js",
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+};
